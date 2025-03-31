1df74acc9a410361ddf61493f27395bdd19ba2f5
yg2127 <yg2127@naver.com>
2025-03-31 17:50:30 +0900

    자료구조 4주차

--- 4-5.c
+++ 4-5.c
@@ -1,3 +1,29 @@
-//
-// Created by À¯°Ç on 25. 3. 30.
-//
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    int arr[100][100] = {0};
+    scanf("%d %d", &n, &m);
+
+    int val = 1;
+    for (int k = 0; k < n + m - 1; k++) {
+        int x = (k < m) ? 0 : k - m + 1;
+        int y = (k < m) ? k : m - 1;
+
+        while (x < n && y >= 0) {
+            arr[x][y] = val;
+            val++;
+            x++;
+            y--;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
