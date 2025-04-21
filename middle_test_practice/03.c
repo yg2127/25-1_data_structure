053d77ed81cb4c2e70f060601b99fb490e5e9c29
yg2127 <yg2127@naver.com>
2025-04-21 20:12:42 +0900

    3번 실습예시

--- middle_test_practice/03.c
+++ middle_test_practice/03.c
@@ -1,3 +1,31 @@
 //
 // Created by 유건 on 25. 4. 19.
 //
+#include<stdio.h>
+
+int gcd1(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd1(b, a % b);
+}
+ing gcd2 (int a, int b) {
+    if (a == b) {
+        return a;
+    }
+    else if (a > b) {
+        return gcd2(a-b,b);
+    }
+    else {
+        return gcd2(a, a-b);
+    }
+}
+int main() {
+    int n1, n2;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+// a는 b보다 무조건 커야 함
+
+    printf("%d\n", gcd1(n1, n2));
+    printf("%d\n", gcd2(n1,n2));
+}
