f3502cc1e79a6bc34a7d41a9bc9b1914f7c5cecb
yg2127 <yg2127@naver.com>
2025-04-21 21:03:22 +0900

    04

--- middle_test_practice/04.c
+++ middle_test_practice/04.c
@@ -3,12 +3,27 @@
 //
 #include<stdio.h>
 typedef struct dev {
-    int d,
-};
+    int d, r;
+}dev;
+
+dev devider(int a, int b) { // ¸ò, ³ª¸ÓÁö
+    dev result;
+    if (a < b) {
+        result.d = 0;
+        result.r = a;
+        return result;
+    }
+    dev tmp = devider(a-b, b);
+    result.d = tmp.d + 1;
+    result.r = tmp.r;
+    return result;
+}
+
 int main() {
     int n1, n2;
     scanf("%d", &n1);
     scanf("%d", &n2);
 
-
+    dev n = devider(n1,n2);
+    printf("%d %d", dev.d, dev.r);
 }
